b444a400a284c98a5bc32ec71ac692f0d052c540
KhTemur <khozhambetovt@gmail.com>
2025-01-17 10:50:52 +0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+const data = [
+    {
+        "name": "Доллар США",
+        "buy": 12940.00,
+        "sell": 12990.00,
+        "icon": "https://quant.brb.uz/images/currency/usd.png"
+    },
+    {
+        "name": "Евро",
+        "buy": 13000.00,
+        "sell": 13400.00,
+        "icon": "https://quant.brb.uz/images/currency/eur.png"
+    },
+    {
+        "name": "Фунт стерлингов",
+        "buy": 15500.00,
+        "sell": 15900.00,
+        "icon": "https://quant.brb.uz/images/currency/gbp.png"
+    },
+    {
+        "name": "Рубль",
+        "buy": 100.00,
+        "sell": 130.00,
+        "icon": "https://quant.brb.uz/images/currency/rub.png"
+    }
+];
